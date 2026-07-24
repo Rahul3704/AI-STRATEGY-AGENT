31b1a15ea06cc0a573aa085f2b5ea3a9adbdb1e6
Rahul3704 <rahulsinhami5a@gmail.com>
2026-07-24 17:57:27 +0530

    update from local to production code2

--- server/server.js
+++ server/server.js
@@ -6,11 +6,18 @@ const aiRoute=require("./routes/ai");
 
 const app=express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/ai",aiRoute);
 
-app.listen(5000,()=>{
-console.log("Server Running");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server Running on ${PORT}`);
 });
